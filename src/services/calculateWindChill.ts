f393c23261ea2a23a6bf5f25122792554f9a0ce9
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-03-05 14:53:56 -0400

    feat: error on invalid resulting temperature

--- src/services/calculateWindChill.ts
+++ src/services/calculateWindChill.ts
@@ -10,5 +10,10 @@ export function calculateWindChill(
   const v = convertUnit(windSpeed, speedUnit.id, "kph");
   const vP6 = Math.pow(v, 0.16);
   const Tp = 13.12 + 0.6215 * Ta - 11.37 * vP6 + 0.3965 * Ta * vP6;
+  if (Tp > Ta) {
+    throw new Error(
+      "The resulting perceived temperature is higher than the actual temperature."
+    );
+  }
   return [Tp, Tp / Ta];
 }
