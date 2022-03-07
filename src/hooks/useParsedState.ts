747236e64b52a41a3d2907ba4a9206354c17d780
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-03-06 23:55:01 -0400

    clean: remove unused file

--- src/hooks/useParsedState.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export type Parser = (text: string) => string | null | Error;
-
-export function useParsedState(parser: Parser, onError: (err: Error) => void) {
-  const [state, setState] = useState<number | null>();
-}
