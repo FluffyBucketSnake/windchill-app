fa1120dcd1d4a0025279fa7a660cbe81705d2a37
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-03-06 23:22:45 -0400

    refactor: send parsing functions into their own file

---
+++ src/utils/parsing.ts
@@ -0,0 +1,20 @@
+export function parseNumber(text: string, context: string): number {
+  if (!text) {
+    throw new Error(`Input a different ${context} value.`);
+  }
+  if (!text.match(/^\-?\d+(.\d+)?$/)) {
+    throw new Error(`${capitalizeFirst(context)} is not a valid number.`);
+  }
+  return parseFloat(text);
+}
+
+export function parseNonNegative(text: string, context: string): number {
+  const result = parseNumber(text, context);
+  if (result < 0) {
+    throw new Error(`${capitalizeFirst(context)} cannot be negative.`);
+  }
+  return result;
+}
+
+const capitalizeFirst: (text: string) => string = (text) =>
+  `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
